d5fcd07df8656de307eebc34e79c52d7674ca136
Hugh Davidson <hugh.davidson@genero.com>
2021-12-04 12:00:25 +1100

    Fix up Day3

--- Day3.hs
+++ Day3.hs
@@ -11,20 +11,26 @@ getLeastFrequent :: (Eq a, Ord a) => [a] -> a
 getLeastFrequent = head . minimumBy (comparing length) . group . sort
 
 getGammaRate :: (Eq a, Ord a)=> [[a]] -> [a]
+getGammaRate [rate] = rate
 getGammaRate codes = getMostFrequent (head <$> codes) : getGammaRate (tail <$> codes)
 
 getEpsilonRate :: (Eq a, Ord a) => [[a]] -> [a]
+getEpsilonRate [rate] = rate
 getEpsilonRate codes = getLeastFrequent (head <$> codes) : getEpsilonRate (tail <$> codes)
 
 getOxygenGeneratorRate :: [[Char]] -> [Char]
+getOxygenGeneratorRate [rate] = rate
 getOxygenGeneratorRate codes = currentDigit : getOxygenGeneratorRate (tail <$> remainingCodes) where
   currentDigit = getMostFrequent $ head <$> codes
-  remainingCodes = filter (\x -> head x == currentDigit) codes
+  leastFrequent = getLeastFrequent $ head <$> codes
+  remainingCodes = filter (\x -> if currentDigit == leastFrequent then head x == '1' else head x == currentDigit) codes
 
 getCo2Rating :: [[Char]] -> [Char]
-getCo2Rating codes = currentDigit : getOxygenGeneratorRate (tail <$> remainingCodes) where
+getCo2Rating [rate] = rate
+getCo2Rating codes = currentDigit : getCo2Rating (tail <$> remainingCodes) where
   currentDigit = getLeastFrequent $ head <$> codes
-  remainingCodes = filter (\x -> head x == currentDigit) codes
+  mostFrequent = getMostFrequent $ head <$> codes
+  remainingCodes = filter (\x -> if currentDigit == mostFrequent then head x == '0' else head x == currentDigit) codes
 
 bintodec :: [Int] -> Int
 bintodec = foldl' (\y x -> x + 2*y) 0
@@ -51,8 +57,8 @@ main = do
   let codes = lines file
       parseAsDec = bintodec . (digitToInt <$>)
 
-      oxygenGeneratorRate = parseAsDec "010000101111"
-      co2Rate = parseAsDec "101100110111"
+      oxygenGeneratorRate = parseAsDec . getOxygenGeneratorRate
+      co2Rate = parseAsDec . getCo2Rating 
+      answer = (*) <$> co2Rate <*> oxygenGeneratorRate
 
-      -- 1071 -- 3706
-      in print co2Rate
+      in print $ answer codes
