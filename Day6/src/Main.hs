beba216d0bca5adb7570a94bb56de56b335a87a9
Hugh Davidson <hugh.davidson@genero.com>
2021-12-07 09:22:00 +1100

    Complete day 6 pt 1

--- Day6/src/Main.hs
+++ Day6/src/Main.hs
@@ -1,17 +1,30 @@
 module Main where
 
 import Text.Trifecta
+import Data.Maybe
 
 -- Part 1
 
-newtype LanternFish = LanternFish Int deriving (Eq, Show)
+newtype LanternFish = LanternFish Integer deriving (Eq, Show)
 
 ageLanternFish :: LanternFish -> LanternFish
 ageLanternFish (LanternFish n) = if n == 0 then LanternFish 6 else LanternFish (n - 1)
 
 elapseDay :: [LanternFish] -> [LanternFish]
-elapseDay = foldr (\lf@(LanternFish n) lfs -> if n == 0 then LanternFish 8 : ageLanternFish lf : lfs else ageLanternFish : lfs)
+elapseDay = foldr (\lf@(LanternFish n) lfs -> if n == 0 then LanternFish 8 : ageLanternFish lf : lfs else ageLanternFish lf : lfs) []
+
+times :: (a -> a) -> Int -> (a -> a)
+times f n = foldr (.) id (replicate n f)
+ 
+type Input = String
+
+parseInput :: Parser [LanternFish]
+parseInput = do
+  fish <- sepBy integer (char ',')
+  return $ LanternFish <$> fish
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  fish <- fromMaybe [] <$> parseFromFile parseInput "./src/day6.txt"
+
+  print $ length (times elapseDay 80 fish)
