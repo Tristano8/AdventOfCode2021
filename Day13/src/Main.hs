c09e908a57989d6ce75fb1de00f1f898953bd8d8
Hugh Davidson <hugh.davidson@genero.com>
2021-12-14 23:01:24 +1100

    Finish part 2

--- Day13/src/Main.hs
+++ Day13/src/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import Control.Applicative
 import Data.Functor
-import Data.List (foldl')
+import Data.List (foldl', intercalate)
 import Data.Maybe
 import qualified Data.Set as S
 
@@ -48,12 +48,20 @@ part1 (Input p f) = S.size $ performFold (head f) p
 
 -- Part 2
 performAllFolds :: Input -> Paper
-performAllFolds (Input p f) = foldl' (flip performFold) mempty f
+performAllFolds (Input p f) = foldl' (flip performFold) p f
 
-drawResult :: Paper -> String
-drawResult = undefined
+drawResult :: Paper -> [Fold] -> String
+drawResult p f = let maxY = last [y | (Fold Y y) <- f]
+                     maxX = last [x | (Fold X x) <- f]
+               in intercalate "\n" $ do
+  y <- [0..maxY]
+  pure $ do
+    x <- [0..maxX]
+    pure $ if S.member (Point x y) p then '#' else '.'
 
 main :: IO ()
 main = do
-  input <- fromMaybe (Input mempty mempty) <$> parseFromFile parseInput "./src/day13.txt"
+  input@(Input p f) <- fromMaybe (Input mempty mempty) <$> parseFromFile parseInput "./src/day13.txt"
   print $ part1 input
+  let finalPaper = performAllFolds input in
+    putStrLn $ drawResult finalPaper f
