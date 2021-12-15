021668dfbc0b8ac1a8247894aa2a86bd2b741407
Hugh Davidson <hugh.davidson@genero.com>
2021-12-16 08:50:06 +1100

    Finish part 2, Day 14

--- Day14/src/Main.hs
+++ Day14/src/Main.hs
@@ -43,10 +43,39 @@ part1 (Input template rules) = let polymer = (!!10) $ iterate' (`step` rules) te
                                    result = sortOn length frequencies
                                    in length (head (reverse result)) - length (head result)
 
-part2 :: Input -> String
-part2 = undefined
+-- Part 2 - Keep a map of pairs in the template instead of a list
+
+type Template' = M.Map String Int
+type StartChar = Char
+data Input' = Input' Template' Rules StartChar deriving (Show)
+
+parseInput' :: Parser Input'
+parseInput' = do
+  template <- token $ some upper
+  rules <- some $ token parseRule
+
+  let templateMap = M.fromListWith (+) ((\x -> (x, 1)) <$> (getPairs template))
+    in pure $ Input' templateMap (M.fromList rules) (head template)
+
+step' :: Template' -> Rules -> Template'
+step' template rules = M.fromListWith (+) $ do
+                          (pair@(c:c':_), n) <- M.assocs template
+                          let insertChar :: Char
+                              insertChar = fromJust $ M.lookup pair rules
+                          [(c:[insertChar], n), (insertChar:[c'], n)]
+
+count' :: Template' -> StartChar -> Int
+count' t char = maximum charCounts - minimum charCounts where
+  charCounts = M.elems (M.insertWith (+) char 1 (M.fromListWith (+) $ do
+                      (c:c':_, n) <- M.assocs t
+                      [(c', n)]))
+
+                          
+part2 :: Input' -> Int
+part2 (Input' template rules startChar) = let finalTemplate = (!!40) $ iterate' (`step'` rules) template 
+                                          in count' finalTemplate startChar
 
 main :: IO ()
 main = do
-  input@(Input template rules) <- fromMaybe (Input mempty mempty) <$> parseFromFile parseInput "./src/day14.txt"
-  print $ part1 input
+  input@(Input' template rules startChar) <- fromMaybe (Input' mempty mempty ' ') <$> parseFromFile parseInput' "./src/day14.txt"
+  print $ part2 input
